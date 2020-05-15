b8cb200c3a4f574cde73ebf1613a131d3174116e
JohnsonNYC <jkow95@gmail.com>
2020-05-15 10:29:05 -0400

    Completed Deliverables

--- src/Inventory.js
+++ src/Inventory.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Purse from "./Purse"
-import InventoryTile from "./InventoryTile"
+import PotionTile from "./PotionTile"
 
 
 const Inventory = (props) => {
@@ -10,20 +10,20 @@ const Inventory = (props) => {
     return props.inventory.map(item => {
       const potion = props.potions.find(potion => potion.id === item.id)
 
-       return (
-        <InventoryTile potion={potion} sell={props.sell}>
+      return (
+        <PotionTile style="inventory-card" key ={potion.id} potion={potion} sell={props.sell}>
           <h4>{potion.name} x {item.amount}</h4>
-        </InventoryTile>
+        </PotionTile>
       )
-    })  
+    })
   }
 
   return (
     <div className="inventory">
       <h2>Inventory</h2>
-      <Purse stopShake={props.stopShake} shake={props.shake} gold={props.gold}/>
+      <Purse stopShake={props.stopShake} shake={props.shake} gold={props.gold} />
       <div className="inventory-cards">
-       {renderInventory()}
+        {renderInventory()}
       </div>
     </div>
   )
--- src/PotionTile.js
+++ src/PotionTile.js
@@ -7,11 +7,29 @@ Consider what the tiles have in common and what they do not:
 - For things that are the same, copy and pasting works just fine! 
 - For things that are different, consider how you might make use of props and JSX to have the same component behave differently when rendered by different parents 
 */
-
+// INV TILE className="inventory-card"
+// SHOP TILE className="card"
 const PotionTile = (props) => {
+
+
   return (
-    null
+    <div key={props.potion.id} className={props.style}>
+      <div onClick={props.style==='inventory-card'? () => props.sell(props.potion.id): () => props.addToInventory(props.potion.id)} className="image-wrapper">
+        <img className="image" alt={props.potion.name} src={props.potion.image_url} />
+      </div>
+      {props.children}
+    </div>
   )
 }
 
 export default PotionTile
+
+// - App
+//    -Shop
+//        -ShopTile / PotionTile
+//    - Inventory 
+//        - Inventory Tile / PotionTile
+// if classname == inv card
+//    onclick = sell(x)
+//  else
+//    onClick= addToInv
